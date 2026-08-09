d5e23ea4d64baf7a77d53cedfaeaa9de6c4c77cb
Thales Oliveira S. Costa <142448714+ThalesSilva67@users.noreply.github.com>
2026-08-09 01:03:09 -0300

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        HashMap<String, String> map = new HashMap<>();
+        String[] arr = {s, t};
+        boolean anagram = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            char[] chars = arr[i].toCharArray();
+            Arrays.sort(chars);
+            String sortedString = new String(chars);
+            map.put(sortedString, arr[i]);
+        }
+
+        for (String key : map.keySet()) {
+            if (map.get(key).equals(t)) {
+                anagram = true;
+            } else {
+                return false;
+            }
+        }
+
+        return anagram;
+    }
+}
