7ff8cb1bb1a91c94eb9daae28a36d2744bbc375c
Thales Oliveira S. Costa <142448714+ThalesSilva67@users.noreply.github.com>
2026-08-17 23:15:12 -0300

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] alfabeto = new int[26];
+        int[] count = new int[26];
+        int left = 0;
+        int right = 0;
+        boolean permutation = false;
+
+        for (int i = 0; i < s1.length(); i++) {
+            alfabeto[s1.charAt(i) - 'a']++;
+        }
+
+        while (right < s2.length()) {
+            count[s2.charAt(right) - 'a']++;
+
+            if (right - left + 1 == s1.length()) {
+                if (Arrays.equals(alfabeto, count)) {
+                    permutation = true;
+                    break;
+                } else {
+                    count[s2.charAt(left) - 'a']--;
+                    left++;
+                }
+            }
+
+            right++;
+        }
+
+        return permutation;
+    }
+}
